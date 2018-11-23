255dfdf53f9201b71c0bcb9b8f1aefcbfbb5ade5
maddyonline <madhavjha@gmail.com>
2018-11-23 12:49:34 -0800

    A minimalist first step towards CLI app with unit tests

---
+++ ModelVerifyTests/ModelVerifyTests.swift
@@ -0,0 +1,32 @@
+//
+//  ModelVerifyTests.swift
+//  ModelVerifyTests
+//
+//  Created by Madhav Jha on 11/23/18.
+//  Copyright © 2018 Madhav Jha. All rights reserved.
+//
+
+import XCTest
+
+class ModelVerifyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssert(true)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+#  Model Verify
+
+## Setup
+
+
+1. Create a MacOS Command Line application in XCode (using default settings).
+2. Create a new target for unit tests: File -> New -> Target -> macOS UnitTesting bundle
+3. Following this [stackoverflow answer](https://stackoverflow.com/a/29178514/1846021), configure the scheme to make Unit Tests target of original application.
+
+
+
+
+
+
