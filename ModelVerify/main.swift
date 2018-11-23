243fb66b8128c256c58fd975b21aefac3e3491e5
maddyonline <madhavjha@gmail.com>
2018-11-23 12:18:59 -0800

    Initial Commit

---
+++ ModelVerify/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ModelVerify
+//
+//  Created by Madhav Jha on 11/23/18.
+//  Copyright © 2018 Madhav Jha. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
