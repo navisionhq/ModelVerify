467ab53883ef51277b668a5718ac8c237b0cdfec
maddyonline <madhavjha@gmail.com>
2018-11-23 16:20:34 -0800

    Verify resnet model on test image in unit tests

--- ModelVerifyTests/ModelVerifyTests.swift
+++ ModelVerifyTests/ModelVerifyTests.swift
@@ -7,26 +7,78 @@
 //
 
 import XCTest
+import Vision
+import CoreML
 
 class ModelVerifyTests: XCTestCase {
+    var sourceDir : String!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sourceDir = "/" + #file.split(separator: "/").dropLast().dropLast().joined(separator: "/")
+    }
+    
+    func getModelFilePath(filename : String) -> String {
+        let path : String = sourceDir + "/data/models/" + filename
+        return path
+    }
+    
+    func getImageFilePath(filename : String) -> String {
+        let path : String = sourceDir + "/data/images/" + filename
+        return path
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testCanAccessModel() {
+        let modelFilePath = getModelFilePath(filename: "ResNet50.mlmodel")
+        let modelUrl = URL(fileURLWithPath: modelFilePath)
+        let data = try? Data(contentsOf: modelUrl)
+        XCTAssertNotNil(data)
     }
-
-    func testExample() {
-        XCTAssert(true)
+    
+    func testCanAccessImage() {
+        let imageFilePath = getImageFilePath(filename: "IMG_0.jpg")
+        let imageUrl = URL(fileURLWithPath: imageFilePath)
+        let data = try? Data(contentsOf: imageUrl)
+        XCTAssertNotNil(data)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testCanCompileModel() {
+        let modelFilePath = getModelFilePath(filename: "ResNet50.mlmodel")
+        let modelUrl = URL(fileURLWithPath: modelFilePath)
+        guard let compiledUrl = try? MLModel.compileModel(at: modelUrl) else { XCTFail("Faile to compile"); return }
+        guard let modelForTest = try? MLModel(contentsOf: compiledUrl) else { XCTFail("Failed to load compiled model"); return }
+        guard let model = try? VNCoreMLModel(for: modelForTest) else { XCTFail("Failed to convert to VNCoreMLModel"); return }
+        XCTAssertEqual(model.className, "VNCoreMLModel")
+    }
+    
+    func testModelCanPredict() {
+        // Get full paths of files
+        let modelFilePath = getModelFilePath(filename: "ResNet50.mlmodel")
+        let imageFilePath = getImageFilePath(filename: "IMG_0.jpg")
+        
+        // Load image
+        let imageUrl = URL(fileURLWithPath: imageFilePath)
+        guard let data = try? Data(contentsOf: imageUrl) else { XCTFail("Failed to load image"); return }
+        let inputImage = CIImage(data: data)
+        
+        // Load and compile model
+        let modelUrl = URL(fileURLWithPath: modelFilePath)
+        guard let compiledUrl = try? MLModel.compileModel(at: modelUrl) else { XCTFail("Faile to compile"); return }
+        guard let modelForTest = try? MLModel(contentsOf: compiledUrl) else { XCTFail("Failed to load compiled model"); return }
+        guard let model = try? VNCoreMLModel(for: modelForTest) else { XCTFail("Failed to convert to VNCoreMLModel"); return }
+        
+        // Make request and verify
+        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+            if err != nil {
+                XCTFail("err: " + err.debugDescription)
+            }
+            guard let results = finishedReq.results as? [VNClassificationObservation] else { XCTFail("Failed to obtain classification observation"); return }
+            
+            let prediction = results.first!
+            XCTAssertGreaterThan(prediction.confidence, 0.75)
+            XCTAssertEqual(prediction.identifier, "analog_clock")
         }
+        let handler = VNImageRequestHandler(ciImage: inputImage!)
+        try? handler.perform([request])
     }
-
+    
 }
